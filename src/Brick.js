eb71d70010a6c1da0f081b07ea92c7d1bb8dfe37
Vincent Poinpoin <vincentpoinpoin@protonmail.com>
2018-02-10 18:31:29 +0100

    Defining Brick object layout

---
+++ src/Brick.js
@@ -0,0 +1,10 @@
+export class Brick {
+  constructor (posX, posY, color) {
+    this.posX   = posX
+    this.posY   = posY
+    this.color  = color
+    this.exists = true
+    this.width  = 85
+    this.height = 25
+  }
+}
