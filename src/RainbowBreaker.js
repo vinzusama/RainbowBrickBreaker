40e5c4a2b4f8e60b95b7deb1aa3b8e2c07871ef4
Vincent Poinpoin <vincentpoinpoin@protonmail.com>
2018-02-20 23:41:27 +0100

    Using requestAnimationFrame() instead of setInterval to animate.

--- src/RainbowBreaker.js
+++ src/RainbowBreaker.js
@@ -4,8 +4,8 @@ import { Racket }   from './Racket';
 
 export class RainbowBreaker {
     constructor(selector) {
-        this.canvas = document.querySelector(selector);                    // Récupère le canvas HTML passé en arg
-        this.ctx    = this.canvas.getContext('2d');                        // Crée un contexte 2d dans le canvas
+        this.canvas = document.querySelector(selector);  // Récupère le canvas HTML passé en arg
+        this.ctx    = this.canvas.getContext('2d');      // Crée un contexte 2d dans le canvas
         this.bricks = new Array();
         this.grid   = new Grid(10, 6);
         this.racket = new Racket('#FFFFFF');
@@ -15,18 +15,18 @@ export class RainbowBreaker {
     gameInit() {
         this.grid.init(this.ctx);
         this.racket.init(
-            this.canvas.width / 2, 
-            Math.trunc(this.canvas.height - (this.canvas.height / 20)), 
+            this.canvas.width / 2,
+            Math.trunc(this.canvas.height - (this.canvas.height / 20)),
             this.ctx
         );
-        
+
         this.ball.init(
-            this.racket.X, 
-            this.racket.posY - this.ball.radius, 
+            this.racket.X,
+            this.racket.posY - this.ball.radius,
             this.ctx
         );
     }
-    
+
     start() {
         document.addEventListener('keydown', (e) => {
             this.racket.keyDownHandler(e);
@@ -40,14 +40,22 @@ export class RainbowBreaker {
             this.racket.mouseMoveHandler(e, this.canvas);
         });
 
-        setInterval(() => {
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.racket.animate(this.ctx);
-            this.grid.draw(this.ctx);
-            this.ball.animate(this.ctx);
 
-            this.bounce(this.grid.bricks, this.racket, this.ball);
-        }, 10);
+        requestAnimationFrame(() => {
+            this.tick(this);
+        });
+    }
+
+    tick(parent) {
+        parent.ctx.clearRect(0, 0, parent.canvas.width, parent.canvas.height);
+        parent.racket.animate(parent.ctx);
+        parent.grid.draw(parent.ctx);
+        parent.ball.animate(parent.ctx);
+        parent.bounce(parent.grid.bricks, parent.racket, parent.ball);
+
+        requestAnimationFrame(() => {
+            parent.tick(parent);
+        });
     }
 
     bounce(bricks, racket, ball) {
@@ -55,9 +63,9 @@ export class RainbowBreaker {
             ball.bounceX();
         } else if (ball.posY < ball.radius) {
             ball.bounceY();
-        // } else if () {
+            // } else if () {
 
-        }else if (ball.Y > racket.posY) {
+        } else if (ball.Y > racket.posY) {
             if ((ball.X > racket.posX) && (ball.X < racket.posX + racket.width)) {
                 ball.bounceY();
             }
@@ -67,8 +75,22 @@ export class RainbowBreaker {
             }
         }
 
-        
-        
+        for (let col = 0; col < bricks.length; col++) {
+            for (let row = 0; row < bricks[col].length; row++) {
+                let brick = bricks[col][row];
+                if (brick.status === 1) {
+                    // if ( ball.Y < (brick.Y + brick.height) ) {
+                    if (ball.Y > brick.y && ball.Y < brick.y + brick.height && ball.X > brick.x && ball.X < brick.x + brick.width) {
+                        ball.bounceY();
+                        brick.status = 0;
+                        // } else if (ball.X >) {
+
+                    }
+                }
+            }
+        }
+
+
 
         // bricks.forEach(brick => {
         //     // console.log(element.x, element.y);
